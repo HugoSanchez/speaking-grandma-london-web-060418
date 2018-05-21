ab00afa55ab712f11bb593c9016e09e75a675da6
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-21 09:57:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,8 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase 
     # return this string if condition is true 
     puts "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   else 
     # return this string if condition is false 
     puts "HUH?! SPEAK UP, SONNY!"
