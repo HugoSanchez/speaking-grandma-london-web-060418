348af117c892b69a28a32b12b28dfd981ad0176e
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-21 09:55:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,13 @@
-def speak_to_grandma(words)
-  if words.upcase != words
-    return "HUH? SPEAK UP, SONNY!"
-    
-  elsif words.upcase == words
-    return "NO, NOT SINCE 1938!"
-  end 
+def speak_to_grandma(phrase)  
+  # Check if string equals same phrase all upper case letters, which means string is all uppercase
+  if phrase == phrase.upcase 
+    # return this string if condition is true 
+    puts "NO, NOT SINCE 1938!"
+  else 
+    # return this string if condition is false 
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
   
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
